3e1332034c7e947797f31e30633df80532aa1378
Umang Chavan <umangchavan@gmail.com>
2018-04-15 13:45:29 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+main.c for taking in input mazes
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+
+int main() {
+
+  FILE * in;
+  in = fopen("~/Desktop/maze2.txt", "r");
+  
+}
